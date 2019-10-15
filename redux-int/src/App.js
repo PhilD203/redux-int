375fcb1f03eb03aee58f8aac53db58405ffb7bfa
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-15 10:35:53 -0500

    Post Form Component Created

--- redux-int/src/App.js
+++ redux-int/src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 
 
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <PostForm />
+        <hr/>
        <Posts />
         <a
           className="App-link"
