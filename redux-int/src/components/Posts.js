71211550247fe997faf7898aaeec6c5def5bcd02
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-18 18:49:34 -0500

    Redux DevTools Enabled

--- redux-int/src/components/Posts.js
+++ redux-int/src/components/Posts.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postActions';
+
 class Posts extends Component {
-    componentWillMount(){
+    componentWillMount() {
         this.props.fetchPosts();
     }
 
@@ -23,8 +25,13 @@ class Posts extends Component {
     }
 }
 
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
+}
+
 const mapStateToProps = state => ({
     posts: state.posts.items
-}) 
+})
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
