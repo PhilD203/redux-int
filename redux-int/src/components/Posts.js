fdde8148e99f4fe73db483212a886ecf7ae5e5db
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-18 18:35:56 -0500

    Posts coming in from Redux State

--- redux-int/src/components/Posts.js
+++ redux-int/src/components/Posts.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 class Posts extends Component {
+    componentWillMount(){
+        this.props.fetchPosts();
+    }
 
     render() {
 
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -19,6 +23,8 @@ class Posts extends Component {
     }
 }
 
+const mapStateToProps = state => ({
+    posts: state.posts.items
+}) 
 
-
-export default Posts;
+export default connect(mapStateToProps, { fetchPosts })(Posts);
